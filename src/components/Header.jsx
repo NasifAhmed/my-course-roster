26316fe2004c21ca0d529fe839fee53f39ff5ea2
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-09-16 20:30:17 +0600

    Add header and main jsx files with boilerplate code and configure google fonts

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <>
+            <h1 className="text-4xl font-bold text-center my-14">
+                Course Registration
+            </h1>
+        </>
+    );
+}
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App.jsx";
+import Main from "./components/Main.jsx";
+import Header from "./components/Header.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <Header />
+        <Main />
     </React.StrictMode>
 );
