02fa90d21d1181a2906da78a770080648bcc50fe
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-09-16 21:08:21 +0600

    Fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Main from "./components/Main.jsx";
+import Body from "./components/Body.jsx";
 import Header from "./components/Header.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <Header />
-        <Main />
+        <Body />
     </React.StrictMode>
 );
